cee4abe73f6b7e19d0be6ede9b18f4f0ec0d46bd
ivankahnybediuk <56602081+ivankahnybediuk@users.noreply.github.com>
2021-02-09 23:31:40 +0300

    adaptive

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ $('.fifth-block-slider').slick({
     prevArrow: ".prev",
     nextArrow: ".next"
 });
+$('.sec-block-mobile').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: false,
+    variableWidth: true
+ })
+$(".forth-block-table-mobile").slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: false,
+    variableWidth: true
+ })
